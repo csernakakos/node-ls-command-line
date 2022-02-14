6f6c950f06407826c6c94f8309318f213dc50164
csernakakos <csernakakos@gmail.com>
2022-02-14 15:22:36 +0100

    Finished app

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 #!/usr/bin/env node
 const fs = require("fs");
+const chalk = require("chalk");
+const path = require("path");
 
-fs.readdir(process.cwd(), (err, filenames) => {
+const { lstat } = fs.promises;
+
+const targetDir = process.argv[2] || process.cwd();
+console.log(process.argv);
+
+fs.readdir(targetDir, async (err, filenames) => {
     if (err) {
         throw new Error(err);
         return;
-    } else {
-        console.log(filenames);
+    };
+
+    const statPromises = filenames.map(filename => {
+        return lstat(path.join(targetDir, filename));
+    });
+
+    const allStats = await Promise.all(statPromises);
+    
+    for (let stats of allStats) {
+        const index = allStats.indexOf(stats);
+
+        if (stats.isFile()) {
+            console.log(chalk.green(filenames[index]));
+        } else {
+            console.log(chalk.yellow(filenames[index]));
+        }
     }
 });
